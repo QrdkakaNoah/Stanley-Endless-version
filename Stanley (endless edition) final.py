841757c79967955f54004001338172012edb922c
QrdkakaNoah <46796628+QrdkakaNoah@users.noreply.github.com>
2019-01-30 14:41:28 -0700

    Add files via upload

---
+++ Stanley (endless edition) final.py
@@ -0,0 +1,118 @@
+###
+#Noah Gaviña
+#1.18.19
+#Employee #427 simulator endless final
+###
+print("Credit to Davey Wreden for The Stanley Parable which this game is based on.")
+print("Game made by Noah Gaviña.")
+print("Hello Employee #427.")
+while True:
+    import random
+    import string
+    random.choice(string.ascii_lowercase)
+    y=random.choice(string.ascii_lowercase)
+    print("Press the " +y+ " key.")
+    x=input()
+    if x == y:
+        import random
+        import string
+        random.choice(string.ascii_lowercase)
+        y=random.choice(string.ascii_lowercase)
+        print("Press the " +y+ " key.")
+        x=input()
+        if x == y:
+            import random
+            import string
+            random.choice(string.ascii_lowercase)
+            y=random.choice(string.ascii_lowercase)
+            print("Press the " +y+ " key.")
+            x=input()
+            if x == y:
+                import random
+                import string
+                random.choice(string.ascii_lowercase)
+                y=random.choice(string.ascii_lowercase)
+                print("Press the " +y+ " key.")
+                x=input()
+                if x == y:
+                    import random
+                    import string
+                    random.choice(string.ascii_lowercase)
+                    y=random.choice(string.ascii_lowercase)
+                    print("Press the " +y+ " key.")
+                    x=input()
+                    if x == y:
+                        import random
+                        import string
+                        random.choice(string.ascii_lowercase)
+                        y=random.choice(string.ascii_lowercase)
+                        print("Press the " +y+ " key.")
+                        x=input()
+                        if x == y:
+                            import random
+                            import string
+                            random.choice(string.ascii_lowercase)
+                            y=random.choice(string.ascii_lowercase)
+                            print("Press the " +y+ " key.")
+                            x=input()
+                            if x == y:
+                                import random
+                                import string
+                                random.choice(string.ascii_lowercase)
+                                y=random.choice(string.ascii_lowercase)
+                                print("Press the " +y+ " key.")
+                                x=input()
+                                if x == y:
+                                    import random
+                                    import string
+                                    random.choice(string.ascii_lowercase)
+                                    y=random.choice(string.ascii_lowercase)
+                                    print("Press the " +y+ " key.")
+                                    x=input()
+                                    if x == y:
+                                        import random
+                                        import string
+                                        random.choice(string.ascii_lowercase)
+                                        y=random.choice(string.ascii_lowercase)
+                                        print("Press the " +y+ " key.")
+                                        x=input()
+                                    else:
+                                        print("You are fired employee #427.")
+                                        print("Input anything to restart.")
+                                        input()
+                                else:
+                                    print("You are fired employee #427.")
+                                    print("Input anything to restart.")
+                                    input()
+                            else:
+                                print("You are fired employee #427.")
+                                print("Input anything to restart.")
+                                input()
+                        else:
+                            print("You are fired employee #427.")
+                            print("Input anything to restart.")
+                            input()
+                    else:
+                        print("You are fired employee #427.")
+                        print("Input anything to restart.")
+                        input()
+                else:
+                    print("You are fired employee #427.")
+                    print("Input anything to restart.")
+                    input()
+            else:
+                print("You are fired employee #427.")
+                print("Input anything to restart.")
+                input()
+        else:
+            print("You are fired employee #427.")
+            print("Input anything to restart.")
+            input()
+    else:
+        print("You are fired employee #427.")
+        print("Input anything to restart.")
+        input()
+else:
+    print("You are fired employee #427.")
+    print("Input anything to restart.")
+    input()
